8c9773766851b756d11d48e55b0abfe505816bca
Liangliang Nan <liangliang.nan@gmail.com>
2019-03-19 21:00:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,13 +32,13 @@ int main(int /*argc*/, char** /*argv*/) {
     // we first add some vertices (in this simple example only three vertices),
     // then we connect them by an edge. The graph looks like:
     //
-    //                  v0     10
-    //                  ------------ v1
-    //                              /
-    //                            /
-    //                          /  20
-    //                        /
-    //                       v2
+    //    v0     10
+    //    ------------ v1
+    //                /
+    //              /
+    //            /  20
+    //          /
+    //         v2
 
     //-----------------------------------------------------------------------
     //  add a vertex to the Graph: add_vertex()
@@ -47,16 +47,19 @@ int main(int /*argc*/, char** /*argv*/) {
     vp0.value = 0;
     vp0.name = "v0";
     VertexDescriptor vd0 = boost::add_vertex(vp0, graph);
+    std::cout << "added vertex " << vd0 << std::endl;
 
     VertexProperty vp1;
     vp1.value = 1;
     vp1.name = "v1";
     VertexDescriptor vd1 = boost::add_vertex(vp1, graph);
+    std::cout << "added vertex " << vd1 << std::endl;
 
     VertexProperty vp2;
     vp2.value = 2;
     vp2.name = "v2";
     VertexDescriptor vd2 = boost::add_vertex(vp2, graph);
+    std::cout << "added vertex " << vd2 << std::endl;
 
     //-----------------------------------------------------------------------
     //  add an edge to the Graph: add_vertex()
@@ -67,18 +70,18 @@ int main(int /*argc*/, char** /*argv*/) {
     std::pair<EdgeDescriptor, bool> status01 = boost::add_edge(vd0, vd1, ep01, graph);
     EdgeDescriptor ed01 = status01.first; // this is how to access the returned edge descriptor
     if (status01.second)
-        std::cout << "edge added connecting v0 and v1" << std::endl;
+        std::cout << "edge " << ed01 << " added connecting " << vd0 << " and " << vd1 << std::endl;
     else
-        std::cout << "failed to add an edge connecting v0 and v1" << std::endl;
+        std::cout << "failed to add an edge connecting " << vd0 << " and " << vd1 << std::endl;
 
     EdgeProperty ep12; // the edge connecting vertex 1 and 2
     ep12.weight = 20.0f;
     std::pair<EdgeDescriptor, bool> status12 = boost::add_edge(vd1, vd2, ep12, graph);
     EdgeDescriptor ed12 = status12.first; // this is how to access the returned edge descriptor
     if (status12.second)
-        std::cout << "edge added connecting v1 and v2" << std::endl;
+        std::cout << "edge " << ed12 << " added connecting " << vd1 << " and " << vd2 << std::endl;
     else
-        std::cout << "failed to add an edge connecting v1 and v2" << std::endl;
+        std::cout << "failed to add an edge connecting " << vd1 << " and " << vd2 << std::endl;
 
     //-----------------------------------------------------------------------
     //  query the number of vertices in a graph
@@ -100,7 +103,7 @@ int main(int /*argc*/, char** /*argv*/) {
         //  access the vertex property given the vertex descriptor
         //-----------------------------------------------------------------------
         const VertexProperty&  vp = graph[vd];  // vp is the vertex property
-        std::cout << "vertex " << vp.name << " has a value " << vp.value << std::endl;
+        std::cout << "vertex " << vd << ": name is " << vp.name << ", value is " << vp.value << std::endl;
      }
 
     //-----------------------------------------------------------------------
@@ -108,12 +111,12 @@ int main(int /*argc*/, char** /*argv*/) {
     //-----------------------------------------------------------------------
     std::pair<EdgeIterator, EdgeIterator> ei = boost::edges(graph);
     for (EdgeIterator eit = ei.first; eit != ei.second; ++eit) {
-       EdgeDescriptor  ed = *eit;               // ed is the edge descriptor
+       EdgeDescriptor ed = *eit;               // ed is the edge descriptor
        //-----------------------------------------------------------------------
        //  access the edge property given the edge descriptor
        //-----------------------------------------------------------------------
        const EdgeProperty&  ep = graph[ed];     // ep is the edge property
-       std::cout << "edge weight: " << ep.weight << std::endl;
+       std::cout << "edge " << ed << ": weight is " << ep.weight << std::endl;
     }
 
     //-----------------------------------------------------------------------
@@ -121,10 +124,8 @@ int main(int /*argc*/, char** /*argv*/) {
     //-----------------------------------------------------------------------
     VertexDescriptor sd = source(ed12, graph);
     VertexDescriptor td = target(ed12, graph);
-    const VertexProperty& s_vp = graph[sd]; // the vertex property of the source vertex
-    const VertexProperty& t_vp = graph[td]; // the vertex property of the target vertex
-    std::cout << "source vertex of the edge: " << s_vp.name << std::endl;
-    std::cout << "target vertex of the edge: " << t_vp.name << std::endl;
+    std::cout << "source vertex of the edge " << ed12 << " is " << sd << std::endl;
+    std::cout << "target vertex of the edge " << ed12 << " is " << td << std::endl;
 
     //-----------------------------------------------------------------------
     //  query the edge for a vertex pair.
@@ -135,40 +136,40 @@ int main(int /*argc*/, char** /*argv*/) {
     std::pair<EdgeDescriptor, bool> test = boost::edge(vd0, vd2, graph);
     if (test.second == true) {
         EdgeDescriptor ed = test.first;
-        const EdgeProperty&  ep = graph[ed];
-        std::cout << "vertex 0 and 2 are connected by an edge with weight " << ep.weight << std::endl;
+        std::cout << "vertex " << vd0 << " and " << vd2 << " are connected by the edge " << ed << std::endl;
      }
     else
-        std::cout << "vertex 0 and 2 are not connected by an edge." << std::endl;
+        std::cout << "vertex " << vd0 << " and " << vd2 << " are not connected by an edge." << std::endl;
 
     test = boost::edge(vd1, vd2, graph);
     if (test.second == true) {
         EdgeDescriptor ed = test.first;
-        const EdgeProperty&  ep = graph[ed];
-        std::cout << "vertex 1 and 2 are connected by an edge with weight " << ep.weight << std::endl;
+        std::cout << "vertex " << vd1 << " and " << vd2 << " are connected by the edge " << ed << std::endl;
      }
     else
-        std::cout << "vertex 1 and 2 are not connected by an edge." << std::endl;
+        std::cout << "vertex " << vd1 << " and " << vd2 << " are not connected by an edge." << std::endl;
 
     //-----------------------------------------------------------------------
     //  access the neighboring vertices of a vertex
     //-----------------------------------------------------------------------
+    std::cout << "neighboring vertices of vertex " << vd1 << ": ";
     std::pair<AdjacentVertexIterator, AdjacentVertexIterator> adj_v_iter = boost::adjacent_vertices(vd1, graph);
     for (AdjacentVertexIterator ait = adj_v_iter.first; ait != adj_v_iter.second; ++ait) {
         VertexDescriptor vd = *ait;
-        const VertexProperty&  vp = graph[vd];
-        std::cout << "vertex " << vp.name << " is adjacent to vertex 1. Its value is " << vp.value << std::endl;
-     }
+        std::cout << vd << " ";
+    }
+    std::cout << std::endl;
 
     //-----------------------------------------------------------------------
     //  access the incicent edges of a vertex
     //-----------------------------------------------------------------------
+    std::cout << "incident edges of vertex " << vd1 << ": ";
     std::pair<OutEdgeIterator, OutEdgeIterator> adj_e_iter = boost::out_edges(vd1, graph);
     for (OutEdgeIterator ait = adj_e_iter.first; ait != adj_e_iter.second; ++ait) {
         EdgeDescriptor ed = *ait;
-        const EdgeProperty&  ep = graph[ed];
-        std::cout << "incient edge of vertex 1 has weight: " << ep.weight << std::endl;
-     }
+        std::cout << ed << " ";
+    }
+    std::cout << std::endl;
 
     return EXIT_SUCCESS;
 }
